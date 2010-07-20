76637a97c44755d9eab1a9718b5528f8d382afba
Kenneth Tilton <kentilton@gmail.com>
2010-07-20 05:15:54 -0400

    nada

--- features.lisp
+++ features.lisp
@@ -4,8 +4,5 @@
   (setf *features* (remove  :its-alive! *features*)))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (setf *features* (remove  :rms-s3 *features*)))
-
-(eval-when (:compile-toplevel :load-toplevel :execute)
   (setf *features* (remove  :debugging-alive! *features*)))
 
