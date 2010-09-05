6b330a682e59864778b24fa91248cf6fa65c889c
Kenneth Tilton <kentilton@gmail.com>
2010-09-05 18:51:09 -0400

    nada much

--- datetime.lisp
+++ datetime.lisp
@@ -118,6 +118,14 @@ See the Lisp Lesser GNU Public License for more details.
       (mod hours 12) minutes
       (if (>= hours 12) "PM" "AM"))))
 
+(defun mm-ss (&optional (i-time (get-universal-time)))
+  (multiple-value-bind
+        (seconds minutes)
+      (decode-universal-time i-time)
+    
+    (format nil "~2,d:~2,'0d"
+      minutes seconds)))
+
 (defun u-date (&optional (i-time (get-universal-time)))
   (multiple-value-bind
         (seconds minutes hours date month year day-of-week daylight-saving-time-p time-zone)
@@ -133,7 +141,7 @@ See the Lisp Lesser GNU Public License for more details.
       )))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (export '(u-day multiple-value-bind m/d/y mm/dd yyyy-mm-dd)))
+  (export '(u-day mm-ss multiple-value-bind m/d/y mm/dd yyyy-mm-dd)))
 
 (defun u-day (&optional (i-time (get-universal-time)))
   (multiple-value-bind
